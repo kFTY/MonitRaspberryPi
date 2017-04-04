5b4f91421593e9b2d83c5937b0818f8c010d29ae
kFTY <diyanchn@gmail.com>
2017-04-04 17:26:25 +0200

    format PEP8

--- RaspberryPiTemperature.py
+++ RaspberryPiTemperature.py
@@ -1,22 +1,25 @@
 from subprocess import check_output
 from re import findall
-from time import sleep, strftime, time
+from time import strftime
+import os
+
 
 def get_temp():
-        temp = check_output(["vcgencmd", "measure_temp"])
-        temp = temp.decode("UTF-8")
-        temp = findall("\d+\.\d+", temp)
-        temp = temp[0]
-        temp = float(temp)
-        return(temp)
+    temp = check_output(["vcgencmd", "measure_temp"])
+    temp = temp.decode("UTF-8")
+    temp = findall("\d+\.\d+", temp)
+    temp = temp[0]
+    temp = float(temp)
+    return(temp)
+
 
 def write_temp(temp):
     with open("cpu_temp.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp)))
 
 
 temp = get_temp()
 write_temp(temp)
 
 if temp > 75:
-        os.system('/usr/bin/pushbullet.sh "RP is very hot!!! %s Degree!!!"' % temp)
+    os.system('/usr/bin/pushbullet.sh "RP is %s Degree!!!"' % temp)
